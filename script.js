c47894d9674c2f8192061fc2e6d6223be8517706
abdul666333 <abdalalkawasmeh@cmail.carleton.ca>
2024-06-09 23:46:27 -0400

    project 1

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+    function showtext(id){
+        const p1=document.getElementById(id);
+
+        
+
+        if (p1.style.display=="none"){
+            p1.style.display="block";
+
+
+        }
+        else{
+            p1.style.display="none"
+        }
+    }
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+
